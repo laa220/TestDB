88d6eca9a851f6e4eecabf5dab3a034f35021637
laa220 <liialabaco@gmail.com>
2021-09-20 21:55:24 +0700

    #15 добавлена процедура sp_Persons_SelectList

---
+++ TestDB/Stored Procedures/sp_Persons_SelectList.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[sp_Persons_SelectList]	
+AS
+begin
+	-- set nocount on запрещает вывод количества строк, на которые влияет
+	-- инструкция Transact-SQL, в составе результатов данных
+	set nocount on
+
+	SELECT [Persons].[Id]
+	,[Persons].[Surname] + N' ' + [Persons].[Name] + isnull(N' ' + [Persons].[Patronymic], N'')
+	from [dbo].[Persons]
+end
